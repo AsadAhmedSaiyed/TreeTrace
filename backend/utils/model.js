fe35cb2141003d06589809e4d687d78167d98f07
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-02-25 15:36:38 +0530

    debugging

--- backend/utils/model.js
+++ backend/utils/model.js
@@ -6,6 +6,7 @@ const openrouter = createOpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENAI_API_KEY, 
 });
-
+console.log(openrouter);
+console.log(process.env.OPENAI_API_KEY);
 // Using Gemini 2.0 Flash (Stable and supported via OpenRouter)
 export const model = openrouter("google/gemini-2.5-flash");
