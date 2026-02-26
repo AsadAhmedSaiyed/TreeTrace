78260c7310682c487834a76d7bb786b90bc64fd5
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-02-26 21:42:16 +0530

    debugging

--- backend/utils/model.js
+++ backend/utils/model.js
@@ -2,7 +2,7 @@ import { ollama } from 'ai-sdk-ollama';
 
 // Add the 'export' keyword directly to the const
 export const model = ollama('llama3.2:3b', { 
-  baseURL: 'http://ollama-service:11434/api', 
+  baseURL: 'http://ollama-service:80/api', 
 });
 
 console.log("✅ Model initialized and exported as named export.");
