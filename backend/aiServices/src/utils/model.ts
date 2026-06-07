9292a4378acaf487187c32f6eeffe5e8e373fd58
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-06-07 12:28:23 +0530

    changing model settings

--- backend/aiServices/src/utils/model.ts
+++ backend/aiServices/src/utils/model.ts
@@ -5,7 +5,10 @@ console.log(process.env.GOOGLE_API_KEY);
 export const model = new ChatGoogle({
     model : "gemini-2.5-flash",
     temperature: 0.2,
-    maxRetries: 3
+    maxRetries: 3,
+    thinkingConfig: {
+    thinkingBudget: 0,  // disables thinking → ~1-2s
+  },
 });
 
 export type {BaseChatModel} from "@langchain/core/language_models/chat_models";
